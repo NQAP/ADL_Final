e7ec79ebe0f77adbff55af697a5c17d8593cc70c
我可能很廢 <nicks96432@gmail.com>
2024-12-16 18:38:51 +0800

    classification (#3)
    * :sparkles: Gemma2 classification 0.68934
    
    * :sparkles: Add output path
    
    * :recycle: Vendor RAG class since we cannot change utils.py
    
    * :sparkles: Add tensorrt and nf4
    
    * :bug: Fix FileNotFoundError
    
    * :sparkles: MAM-selfICL implementation
    
    * :sparkles: Add option to save memory
    
    * :zap: Don't clear memory when only one model exists
    
    * :sparkles: Set global seed
    
    * :speech_balloon: Swap zero and few shot prompt

--- main.py
+++ main.py
@@ -1,21 +1,101 @@
+import gc
 import random
 import re
+import textwrap
 import warnings
 from abc import ABC, abstractmethod
 from typing import override
 
+import faiss
+import numpy
 import torch
 from accelerate import Accelerator
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import (
+    AutoModel,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    set_seed,
+)
 from transformers import logging as transformers_logging
 
 from base import Agent
 from execution_pipeline import main
-from utils import RAG, strip_all_lines
+from utils import RetrieveOrder
 
 # Ignore warning messages from transformers
 warnings.filterwarnings("ignore")
 transformers_logging.set_verbosity_error()
+
+
+class RAG:
+    def __init__(self, rag_config: dict) -> None:
+        self.embed_model = AutoModel.from_pretrained(
+            rag_config["embedding_model"],
+            **rag_config["embedding_model_kwargs"],
+            trust_remote_code=True,
+        ).eval()
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            rag_config["embedding_model"], trust_remote_code=True
+        )
+
+        self.index = None
+        self.id2evidence = {}
+        self.embed_dim = len(self.encode_data("Test embedding size"))
+        self.insert_acc = 0
+
+        self.top_k = rag_config["top_k"]
+        orders = {member.value for member in RetrieveOrder}
+        assert rag_config["order"] in orders
+        self.retrieve_order = rag_config["order"]
+
+        self.create_faiss_index()
+        # TODO: make a file to save the inserted rows
+
+    def create_faiss_index(self):
+        self.index = faiss.IndexFlatL2(self.embed_dim)
+
+    def encode_data(self, sentence: str) -> numpy.ndarray:
+        # Tokenize the sentence
+        encoded_input = self.tokenizer(
+            [sentence], padding=True, truncation=True, return_tensors="pt"
+        )
+        # Compute token embeddings
+        with torch.inference_mode():
+            model_output = self.embed_model(**encoded_input)
+            # Perform pooling. In this case, cls pooling.
+            sentence_embeddings = model_output[0][:, 0]
+        feature = sentence_embeddings.numpy()[0]
+        norm = numpy.linalg.norm(feature)
+        return feature / norm
+
+    def insert(self, key: str, value: str) -> None:
+        """Use the key text as the embedding for future retrieval of the value text."""
+        embedding = self.encode_data(key).astype("float32")  # Ensure the data type is float32
+        self.index.add(numpy.expand_dims(embedding, axis=0))
+        self.id2evidence[str(self.insert_acc)] = value
+        self.insert_acc += 1
+
+    def retrieve(self, query: str, top_k: int) -> list[str]:
+        """Retrieve top-k text chunks"""
+        embedding = self.encode_data(query).astype("float32")  # Ensure the data type is float32
+        top_k = min(top_k, self.insert_acc)
+        distances, indices = self.index.search(numpy.expand_dims(embedding, axis=0), top_k)
+        distances = distances[0].tolist()
+        indices = indices[0].tolist()
+
+        results = [
+            {"link": str(idx), "_score": {"faiss": dist}}
+            for dist, idx in zip(distances, indices, strict=False)
+        ]
+        # Re-order the sequence based on self.retrieve_order
+        if self.retrieve_order == RetrieveOrder.SIMILAR_AT_BOTTOM.value:
+            results = list(reversed(results))
+        elif self.retrieve_order == RetrieveOrder.RANDOM.value:
+            random.shuffle(results)
+
+        text_list = [self.id2evidence[result["link"]] for result in results]
+        return text_list
 
 
 class LocalModelAgent(Agent, ABC):
@@ -33,34 +113,69 @@ class LocalModelAgent(Agent, ABC):
 
         self.accelerator = Accelerator(dynamo_backend=self.llm_config["dynamo_backend"])
 
-        bnb_config = BitsAndBytesConfig(load_in_8bit=True)
+        self.current_model_index = 0
+        self.models = {}
+        self.tokenizers = {}
 
-        self.model = AutoModelForCausalLM.from_pretrained(
-            self.llm_config["model_name"],
-            quantization_config=bnb_config,
-            device_map="cuda:0",
-        )
-        self.model.eval()
-        self.model = self.accelerator.prepare(self.model)
-
-        self.tokenizer = AutoTokenizer.from_pretrained(self.llm_config["model_name"])
+        if not self.llm_config["save_memory"] or len(self.llm_config["model_names"]) == 1:
+            for i, _ in enumerate(self.llm_config["model_names"]):
+                self.prepare_model(i)
 
         self.inputs = []
         self.self_outputs = []
         self.rag = RAG(self.llm_config["rag"])
+
+    def prepare_model(self, index: int) -> None:
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+
+        if self.llm_config["save_memory"]:
+            for model in self.models.values():
+                self.accelerator.free_memory(model)
+                del model
+
+            self.models.clear()
+            self.tokenizers.clear()
+            torch.cuda.empty_cache()
+            gc.collect()
+
+        model_name = self.llm_config["model_names"][index]
+
+        self.models[model_name] = self.accelerator.prepare(
+            AutoModelForCausalLM.from_pretrained(
+                model_name,
+                quantization_config=bnb_config,
+                device_map="cuda:0",
+                trust_remote_code=True,
+            )
+        )
+        self.tokenizers[model_name] = AutoTokenizer.from_pretrained(
+            model_name, trust_remote_code=True
+        )
 
     def generate_response(self, messages: list[dict[str, str]]) -> str:
         """
         Generate a response using the local model.
         """
 
-        text_chat: str = self.tokenizer.apply_chat_template(  # type: ignore
+        if self.llm_config["save_memory"] and len(self.llm_config["model_names"]) > 1:
+            self.prepare_model(self.current_model_index)
+            assert len(self.models) == 1
+
+        model_name = self.llm_config["model_names"][self.current_model_index]
+        current_model, current_tokenizer = self.models[model_name], self.tokenizers[model_name]
+
+        text_chat: str = current_tokenizer.apply_chat_template(  # type: ignore
             messages, tokenize=False, add_generation_prompt=True
         )
-        model_inputs = self.tokenizer([text_chat], return_tensors="pt").to(self.model.device)
+        model_inputs = current_tokenizer([text_chat], return_tensors="pt").to(current_model.device)
 
         with torch.inference_mode():
-            generated_ids = self.model.generate(
+            generated_ids = current_model.generate(
                 **model_inputs, max_new_tokens=self.llm_config["max_tokens"], do_sample=False
             )
 
@@ -69,7 +184,18 @@ class LocalModelAgent(Agent, ABC):
             for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids, strict=False)
         ]
 
-        return self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        response = current_tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+        self.current_model_index = (self.current_model_index + 1) % len(
+            self.llm_config["model_names"]
+        )
+
+        if self.llm_config["save_memory"]:
+            del generated_ids
+            torch.cuda.empty_cache()
+            gc.collect()
+
+        return response
 
     @override
     def update(self, correctness: bool) -> bool:
@@ -83,6 +209,7 @@ class LocalModelAgent(Agent, ABC):
         Returns:
             bool: Whether the prediction is correct.
         """
+
         if correctness:
             question = self.inputs[-1]
             answer = self.self_outputs[-1]
@@ -103,18 +230,16 @@ class ClassificationAgent(LocalModelAgent):
 
     @staticmethod
     def get_system_prompt() -> str:
-        system_prompt = """
+        return textwrap.dedent("""
         Act as a professional medical doctor that can diagnose the patient
         based on the patient profile. Provide your diagnosis in the following
         format: <number>. <diagnosis>
-        """.strip()
-
-        return strip_all_lines(system_prompt)
+        """).strip()
 
     @staticmethod
     def get_zeroshot_prompt(option_text: str, text: str) -> str:
-        prompt = f"""
-        Act as a medical doctor and diagnose the patient based on the following patient profile:
+        return textwrap.dedent(f"""
+        Diagnose the patient based on the following patient profile:
 
         {text}
 
@@ -123,19 +248,19 @@ class ClassificationAgent(LocalModelAgent):
 
         {option_text}
 
-        Now, directly provide the diagnosis for the patient in the following format:
+        Even if there's no diagnosis that perfectly matches the patient profile, you should still
+        choose the most possible one from the list. Now, directly provide the diagnosis for the
+        patient in the following format:
         <number>. <diagnosis>
-        """.strip()
-
-        return strip_all_lines(prompt)
+        """).strip()
 
     @staticmethod
     def get_fewshot_template(
         option_text: str,
         text: str,
     ) -> str:
-        prompt = f"""
-        Act as a medical doctor and diagnose the patient based on the provided patient profile.
+        return textwrap.dedent(f"""
+        Diagnose the patient based on the provided patient profile.
 
         All possible diagnoses for you to choose from are as follows (one diagnosis per line,
         in the format of <number>. <diagnosis>):
@@ -150,9 +275,7 @@ class ClassificationAgent(LocalModelAgent):
         {text}
 
         Now provide the diagnosis for the patient in the following format: <number>. <diagnosis>
-        """.strip()
-
-        return strip_all_lines(prompt)
+        """).strip()
 
     @staticmethod
     def extract_label(pred_text: str, label2desc: dict[int, str]) -> str:
@@ -176,12 +299,12 @@ class ClassificationAgent(LocalModelAgent):
 
     @staticmethod
     def get_shot_template() -> str:
-        prompt = """
+        prompt = textwrap.dedent("""
         {question}
         Diagnosis: {answer}
-        """.strip()
+        """).strip()
 
-        return strip_all_lines(prompt)
+        return prompt
 
     @override
     def __call__(self, label2desc: dict[int, str], text: str) -> str:
@@ -231,15 +354,14 @@ class ClassificationAgent(LocalModelAgent):
             prompt = prompt_zeroshot
 
         messages = [
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": prompt},
+            {"role": "user", "content": f"{system_prompt}\n{prompt}"},
         ]
         response = self.generate_response(messages)
         prediction = self.extract_label(response, label2desc)
 
         self.update_log_info(
             log_data={
-                "input_pred": messages[1]["content"],
+                "input_pred": messages[0]["content"],
                 "output_pred": response,
                 "num_shots": str(len(shots)),
             }
@@ -285,6 +407,9 @@ class SQLGenerationAgent(Agent):
 
 
 if __name__ == "__main__":
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.set_float32_matmul_precision("high")
+
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
@@ -301,22 +426,33 @@ if __name__ == "__main__":
         msg = f"Invalid benchmark name: {args.bench_name}"
         raise ValueError(msg)
 
-    model_name = "Qwen/Qwen2.5-7B-Instruct"
-
-    bench_cfg = {"bench_name": args.bench_name}
     config = {
-        **bench_cfg,
-        "dynamo_backend": "inductor",
-        "exp_name": f"self_streamicl_{args.bench_name}_{model_name}_8bit",
-        "bench_namw": bench_cfg["bench_name"],
-        "model_name": model_name,
+        "save_memory": True,
+        "dynamo_backend": "tensorrt",
+        "exp_name": f"mam_streamicl_{args.bench_name}_nf4",
+        "bench_name": args.bench_name,
+        "model_names": ["Qwen/Qwen2.5-7B-Instruct", "google/gemma-2-9b-it"],
         "max_tokens": max_tokens,
+        "seed": 0,
         "rag": {
             "embedding_model": "dunzhang/stella_en_400M_v5",
-            "seed": 0,
             "top_k": 5,
-            "order": "similar_at_top",
+            "order": "similar_at_bottom",
+            "embedding_model_kwargs": {
+                "use_memory_efficient_attention": False,
+                "unpad_inputs": False,
+            },
         },
     }
+    bench_cfg = {
+        "bench_name": args.bench_name,
+        "output_path": f"{args.bench_name}/{config['exp_name']}.csv",
+    }
+
+    set_seed(config["seed"])
+
+    if config["dynamo_backend"] == "tensorrt":
+        import torch_tensorrt  # noqa: F401
+
     agent = agent_name(config)
     main(agent, bench_cfg, use_wandb=True, wandb_name=config["exp_name"], wandb_config=config)
